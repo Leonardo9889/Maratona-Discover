57382060bd211edbceaf9b3b288530dc200b3a4c
Leonardo <leonardoalmeida1301@hotmail.com>
2021-06-09 22:30:15 -0300

    Created jobs object for test

--- maratona-discover-02-1.0.0/src/routes.js
+++ maratona-discover-02-1.0.0/src/routes.js
@@ -5,7 +5,7 @@ const views = __dirname + "/views/"
 
 
 
-const profile = {
+const profile = { // objeto de teste para enviar no HTML
     name:"Leonardo",
     avatar:"https://avatars.githubusercontent.com/u/49963277?v=4",
     "monthly-budget": 3000,
@@ -14,11 +14,43 @@ const profile = {
     "vacation-per-year": 4
 }
 
+
+const jobs = [
+    {
+        id: 1,
+        name: "Pizzaria Guloso",
+        "daily-hours": 2, 
+        "total-hours": 60,
+        createdAt: Date.now()  
+    },
+    {
+        id: 2,
+        name: "OneTwo Project",
+        "daily-hours": 3, 
+        "total-hours": 47,
+        createdAt: Date.now()  
+    }
+]; // Array de jobs
+
 routes
 //Request, Response
-.get('/', (req,res) =>res.render(views + "index"))
-.get('/job', (req,res) =>res.render(views + "job"))
-.get('/job/edit', (req,res) =>res.render(views + "job-edit"))
-.get('/profile', (req,res) =>res.render(views + "profile", {profile}))
+.get('/', (req,res) =>res.render(views + "index", {jobs})) // Renderizando a página index
+.get('/job', (req,res) =>res.render(views + "job")) // Renderizando a página job
+.post('/job', (req,res) => {
+    // req.body = { name: 'fdaf', 'daily-hours': '2', 'total-hours': '4' }
 
-module.exports = routes;
+    const lastId = jobs[jobs.length -1]?.id ||1 // Verifica se no array possui um valor inserido
+
+   jobs.push({ // Enviando o objeto body para o array jobs;
+        id: lastId + 1,
+        name: req.body.name,
+        "daily-hours": req.body["daily-hours"], 
+        "total-hours": req.body["total-hours"],
+        createdAt: Date.now() // Pega a data do agora/hoje
+    })
+    return res.redirect('/') // redirecionando para a página principal
+})
+.get('/job/edit', (req,res) =>res.render(views + "job-edit")) // renderizando a página job-edit
+.get('/profile', (req,res) =>res.render(views + "profile", {profile})) // Renderizando a página profile
+
+module.exports = routes; // Exportando este arquivo de rotas
--- maratona-discover-02-1.0.0/src/server.js
+++ maratona-discover-02-1.0.0/src/server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const server = express();
 const routes = require('./routes');
-
+const port = 3000;
 
 server
 // Habilitando arquivos static
-.use(express.static("public"))
+.use(express.static("public")) // Usando a pasta "Public" como arquivos estaticos ( não haverá modificação )
 
 //Template engine
-.set('view engine', 'ejs')
+.set('view engine', 'ejs') // Setando a template engine EJS para trabalhar junto do backend
+
+//Usar o req.body 
+.use(express.urlencoded({extended:true})) // Permitindo o uso dos dados enviados do frontend para o backend
 
 // Chamando as rotas
-.use(routes)
+.use(routes) // Usando o arquivos de rotas
 
-server.listen(3000, () => console.log ("Server Runing"));
+server.listen(port, () => console.log (`Server runing on port: ${port}`)); // Iniciando o servidor
