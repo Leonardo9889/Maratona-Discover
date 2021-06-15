ba7c9fa69eb8b9d8744edfe9a8284bb5a78c6b67
Leonardo <leonardoalmeida1301@hotmail.com>
2021-06-15 18:45:25 -0300

    job:Calculate days

--- maratona-discover-02-1.0.0/src/routes.js
+++ maratona-discover-02-1.0.0/src/routes.js
@@ -11,7 +11,8 @@ const profile = { // objeto de teste para enviar no HTML
     "monthly-budget": 3000,
     "hours-per-day": 5,
     "days-per-week": 5,
-    "vacation-per-year": 4
+    "vacation-per-year": 4,
+    "value-hours": 75
 }
 
 
@@ -20,7 +21,7 @@ const jobs = [
         id: 1,
         name: "Pizzaria Guloso",
         "daily-hours": 2, 
-        "total-hours": 60,
+        "total-hours": 1,
         createdAt: Date.now()  
     },
     {
@@ -32,20 +33,41 @@ const jobs = [
     }
 ]; // Array de jobs
 
+function remainingDays(job){ // função para calcular os dias restantes para a entrega do projeto;
+        // Cálculo de tempo restante
+        const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed() // Esta realizando a divisão do total de horas por dias que vai trabalhar, tendo como resultado o dia da entrega ( toFixed() transforma em um valor inteiro)
+
+        const createdDate = new Date(job.createdAt) // Recebe os milisegundos na data de criação, e transforma em uma data
+        const dueDay = createdDate.getDate() + Number(remainingDays) // Soma a data de criação com o tempo restante para a entrega, e resulta nos dias restante para a entrega
+        const dueDateInMs = createdDate.setDate(dueDay) // Transformar a data que falta em milisegundos
+
+        const timeDiffInMs = dueDateInMs - Date.now() // calcula o dia faltante ( em milesegundos ) subtraindo com a data de agora/hoje
+
+        //ransformar milisegundos em dias
+        const dayInMs = 1000 * 60 * 60 * 24 // Calculo para transformar os milesegundos em data
+        const dayDiff = Math.floor(timeDiffInMs / dayInMs) // Tem como resultado os dias faltantes para a conclusão ( Math.floor = arredonda a data em um valor inteiro e para baixo)
+
+        return dayDiff // resultado dos dias restantes
+}
+
 routes
 //Request, Response
 .get('/', (req,res) => {
     
     const updatadJobs = jobs.map((job) => {
         //Ajustes no job
-        // Cálculo de tempo restante
-        const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed() // Esta realizando a divisão do total de horas por dias que vai trabalhar, tendo como resultado o dia da entrega ( toFixed() transforma em um valor inteiro)
+        const remaining = remainingDays(job)
+        const status = remaining <= 0 ? 'done' : 'progress' // Condicional ternaria ( verifica se chegou no dia 0 e adiciona 'done' se chegou ou 'progrss' se nõa chegou)
 
-        const createdDate = new Date(job.createdAt) // Recebe os milisegundos na data de criação, e transforma em uma data
-        const dueDay = createdDate.getDate() + Number(remainingDays) // Soma a data de criação com o tempo restante para a entrega, e resulta nos dias restante para a entrega
+        return {
+            ...job, //espalhamento
+            remaining,
+            status,
+            budget: profile["value-hours"] * job["total-hours"]
+        }
     }) 
 
-   return res.render(views + "index", {jobs})
+   return res.render(views + "index", {jobs: updatadJobs})
 }) // Renderizando a página index
 
 
@@ -53,7 +75,7 @@ routes
 .post('/job', (req,res) => {
     // req.body = { name: 'fdaf', 'daily-hours': '2', 'total-hours': '4' }
 
-    const lastId = jobs[jobs.length -1]?.id ||1 // Verifica se no array possui um valor inserido
+    const lastId = jobs[jobs.length -1]?.id ||1 // Verifica se no array se possui um valor inserido
 
    jobs.push({ // Enviando o objeto body para o array jobs;
         id: lastId + 1,
